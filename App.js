c85b2aeb7e3b8673eb27ce6c119c2784b2699a3e
SDQ\cristian.restituyo <crestituyo@theoreminc.net>
2018-03-02 12:30:14 -0400

    Call API to return the Current Weather and replace static data with dynamic one.

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
 import React from 'react';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, ScrollView, Dimensions } from 'react-native';
 import Temperature from './components/Temperature/Temperature';
 import LocalWidget from './components/LocalWidget/LocalWidget';
 import ExtraInfo from './components/ExtraInfo/ExtraInfo';
-import { Ionicons } from '@expo/vector-icons';
-import { Font } from 'expo';
+import NextDays from './components/NextDays/NextDays';
+import { Font, Permissions, Location } from 'expo';
+import dateFormat from 'dateformat';
+import * as WeatherApi from './Global/WeatherApi';
 
 
 export default class App extends React.Component {
 
   state = {
-    isReady: false
+    isReady: false,
+    location: null,
+    weatherInfo: null,
+    isFarenheit:true,
+    isMiles:true
   };
 
   componentDidMount() {
     this._loadFontsAsync();
+    this._loadLocationAsync();
+ 
+  }
+  _initData = async () => {
+    if (this.state.location) {
+      const locationInfo = {
+        lat: this.state.location.coords.latitude,
+        lgn: this.state.location.coords.longitude
+      };
+
+      let weatherInfo = await WeatherApi.getCurrentWeather(locationInfo);
+      this.setState({weatherInfo:weatherInfo});
+    }
   }
   _loadFontsAsync = async () => {
     await Font.loadAsync({
@@ -23,25 +42,47 @@ export default class App extends React.Component {
     });
     this.setState({ isReady: true });
   }
+  _loadLocationAsync = async () => {
+    let { status } = await Permissions.askAsync(Permissions.LOCATION);
+    if (status !== 'granted') {
+      alert('denied');
+    } else {
+      location = await Location.getCurrentPositionAsync({ enableHighAccuracy: true })
+      this.setState({ location: location }, this._initData);
+    }
+
+  };
   render() {
     let content = null;
-    if (this.state.isReady) {
+   
+    if (this.state.isReady && this.state.weatherInfo) {
+      
       content = (
-        <View>
-          <LocalWidget
-            status="PART_CLOUDY_NIGHT"
-            city='London'
-            date='Wednesday, Sept 25' />
-          <Temperature value={17} />
-          <ExtraInfo />
+        <View style={styles.content}>
+          <View style={styles.topSection}>
+            <LocalWidget
+              status={this.state.weatherInfo.current.condition.text}
+              city={this.state.weatherInfo.location.region}
+              isDay = {!!this.state.weatherInfo.current.is_day}
+              date= {dateFormat(new Date(), "dddd, mmm dS")} />
+            <Temperature value={this.state.isFarenheit ? this.state.weatherInfo.current.temp_f : this.state.weatherInfo.current.temp_c} />
+            <ExtraInfo 
+              windSpeed = {(this.state.isMiles)? this.state.weatherInfo.current.wind_mph + 'mph' : this.state.weatherInfo.current.wind_kph + 'kph'}
+              rainChance = {this.state.weatherInfo.current.pressure_in}
+              humidity = {this.state.weatherInfo.current.humidity}
+            />
+          </View>
+          <View style={styles.footerSection}>
+            <NextDays />
+          </View>
         </View>
-      )
+      );
     }
     return (
       <ImageBackground style={styles.container} source={require('./assets/bg.png')}>
         {content}
       </ImageBackground>
-    )
+    );
   }
 }
 
@@ -51,5 +92,18 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
   },
+  content: {
+    flex: 1,
+    width: '100%'
+  },
+  topSection: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  footerSection: {
+    height: 120,
+  }
 });
